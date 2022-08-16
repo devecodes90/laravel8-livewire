4b3b09e427b9a2fec5ab0ebfdc93e512e4222cff
benaichacodes <deve90codes@yahoo.com>
2022-08-16 07:21:36 +0000

    Mes nouvelles modifications

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -155,14 +155,12 @@
 
 
     <aside class="control-sidebar control-sidebar-dark">
-
-        <div class="p-3">
-            <div class="card card-primary card-outline">
-                <div class="card-body box-profile">
+            <div class="card bg-dark">
+                <div class="card-body bg-dark box-profile">
                     <div class="text-center">
-                        <img class="profile-user-img img-fluid img-circle" src="../../dist/img/user4-128x128.jpg" alt="User profile picture">
+                        <img class="profile-user-img img-fluid img-circle" src="https://cdn-icons.flaticon.com/png/512/1144/premium/1144760.png?token=exp=1660320656~hmac=edaf6dc4d46cfc82ca6975b822f711c2" alt="User profile picture">
                     </div>
-                    <h3 class="profile-username text-center">Nina Mcintire</h3>
+                    <h3 class="profile-username text-center">{{ auth()->user()->nom }} {{ auth()->user()->prenoms }}</h3>
                     <p class="text-muted text-center">Software Engineer</p>
                     <ul class="list-group list-group-unbordered mb-3">
                         <li class="list-group-item">
@@ -179,7 +177,6 @@
                 </div>
 
             </div>
-        </div>
     </aside>
 
 
